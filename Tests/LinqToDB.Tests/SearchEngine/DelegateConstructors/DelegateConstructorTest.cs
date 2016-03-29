73bb9915806b7feed93d2eba1e7ca9edd6b66611
Виктор Борисов <viktor.borisov@bars-open.ru>
2016-03-29 11:19:50 +0600

    добавил тест на ветвления

--- Tests/LinqToDB.Tests/SearchEngine/DelegateConstructors/DelegateConstructorTest.cs
+++ Tests/LinqToDB.Tests/SearchEngine/DelegateConstructors/DelegateConstructorTest.cs
@@ -12,15 +12,31 @@
     {
         private interface IBase
         {
-             
+
         }
+
         private interface IA : IBase
         {
             [SearchContainer]
-            IB B { get; set; } 
+            IB B { get; set; }
         }
 
         private interface IB : IBase
+        {
+            [SearchContainer]
+            IC C { get; set; }
+
+            [SearchContainer]
+            ID D { get; set; }
+        }
+
+        private interface IC : IBase
+        {
+            [SearchContainer]
+            ID D { get; set; }
+        }
+
+        private interface ID : IBase
         {
         }
 
@@ -36,26 +52,82 @@
 
         private class B : IB
         {
+            public IC C { get; set; }
+
+            [SearchContainer]
+            public ID D { get; set; }
+
+            public B()
+            {
+                C = new C();
+                D = new D();
+            }
+        }
+
+        private class C : IC
+        {
+            [SearchContainer]
+            public ID D { get; set; }
+
+            public C()
+            {
+                D = new D();
+            }
+        }
+
+        private class D : ID
+        {
         }
 
         [Fact]
-        public void SimpleDelegate()
+        public void SimpleDelegateB()
         {
-            var typeGraph = new TypeGraph<IBase>(GetType().Assembly.GetTypes());
-
-            var pathBuilder = new PathBuilder<IBase>(typeGraph);
-
             var obj = new A();
-            var paths = pathBuilder.Find(obj, typeof(IB));
+            var deleg = SetupTest<IBase, IB>(obj);
 
-            var delegateConstructor = new DelegateConstructor<IB>();
-            var deleg = delegateConstructor.CreateResultDelegate(paths);
-
-            LinkedList<IB> result = new LinkedList<IB>();
+            var result = new LinkedList<IB>();
             deleg(obj, result);
 
             Assert.Equal(1, result.Count);
             Assert.Equal(obj.B, result.First.Value);
+        }
+
+        [Fact]
+        public void SimpleDelegateC()
+        {
+            var obj = new A();
+            var deleg = SetupTest<IBase, IC>(obj);
+
+            var result = new LinkedList<IC>();
+            deleg(obj, result);
+
+            Assert.Equal(1, result.Count);
+            Assert.Equal(obj.B.C, result.First.Value);
+        }
+
+        [Fact]
+        public void BranchDelegateD()
+        {
+            var obj = new A();
+            var deleg = SetupTest<IBase, ID>(obj);
+
+            var result = new LinkedList<ID>();
+            deleg(obj, result);
+
+            Assert.Equal(2, result.Count);
+            //Assert.Equal(obj.B.C, result.First.Value);
+        }
+
+        private DelegateConstructor<TSearch>.ResultDelegate SetupTest<TBase, TSearch>(TBase obj) where TSearch : class
+        {
+            var typeGraph = new TypeGraph<TBase>(GetType().Assembly.GetTypes());
+
+            var pathBuilder = new PathBuilder<TBase>(typeGraph);
+
+            var paths = pathBuilder.Find(obj, typeof(TSearch));
+
+            var delegateConstructor = new DelegateConstructor<TSearch>();
+            return delegateConstructor.CreateResultDelegate(paths);
         }
     }
 }
