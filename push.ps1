3811f677a77a2d67914c31eab16b5692aea3e0e6
Виктор Борисов <viktor.borisov@bars-open.ru>
2016-05-10 15:47:08 +0600

    Опубликован в myget

--- push.ps1
+++ push.ps1
@@ -5,5 +5,5 @@
 $version = Get-Content .\package.version
 
 foreach ($nugetPackage in Get-ChildItem -Path .\nuget -Filter "*$version.nupkg" ) {
-    .\.paket\paket.exe push url https://barsgroup.myget.org/F/kaliningrad/api/v2/package file $nugetPackage.FullName apikey $apikey
+    .\.paket\paket.exe push url https://barsgroup.myget.org/F/common/api/v2/package file $nugetPackage.FullName apikey $apikey
 }
